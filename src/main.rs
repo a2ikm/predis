8c1854d70fb198e297484e8a5bc81db5e0988548
Masato Ikeda <masato.ikeda@gmail.com>
2025-04-15 00:13:09 +0900

    Implement simple echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::{TcpStream, TcpListener};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8765")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()>{
+    let mut request_buf = [0; 4096];
+    let _size = stream.read(&mut request_buf).unwrap();
+
+    stream.write_all(&request_buf)?;
+    Ok(())
 }
