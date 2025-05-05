8173c5ea74d64589263c25942e0ba9daf5965ded
Masato Ikeda <masato.ikeda@gmail.com>
2025-05-05 12:24:00 +0900

    Respond encoded value

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,40 @@ use std::net::{TcpStream, TcpListener};
 
 mod resp;
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379")?;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        handle_client(stream?)?;
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            },
+            Err(e) => {
+                println!("connection error: {:?}", e);
+            }
+        }
     }
-
-    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()>{
+fn handle_client(mut stream: TcpStream) {
     let mut request_buf = [0; 4096];
     let _size = stream.read(&mut request_buf).unwrap();
 
-    match resp::decode(&request_buf) {
+    let response_buf = handle_request(&request_buf);
+
+    stream.write_all(&response_buf).expect("response_buf should be written");
+}
+
+fn handle_request(request_buf: &[u8]) -> Vec<u8> {
+    let request = match resp::decode(request_buf) {
         Ok(value) => {
-            println!("decode success: {:?}", value);
+            value
         },
         Err(e) => {
-            println!("decode error: {:?}", e);
+            let msg = format!("decode error: {:?}", e);
+            return resp::encode(&resp::Value::SimpleString(msg.as_bytes().to_vec()))
         }
-    }
+    };
 
-    stream.write_all(&request_buf)?;
-    Ok(())
+    resp::encode(&request)
 }
