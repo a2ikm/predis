f73ae87e22b6906a861a1bed5a19f7b79faffb6e
Masato Ikeda <masato.ikeda@gmail.com>
2025-05-05 11:25:04 +0900

    Implement SimpleString decoding

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,15 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()>{
     let mut request_buf = [0; 4096];
     let _size = stream.read(&mut request_buf).unwrap();
 
+    match resp::decode(&request_buf) {
+        Ok(value) => {
+            println!("decode success: {:?}", value);
+        },
+        Err(e) => {
+            println!("decode error: {:?}", e);
+        }
+    }
+
     stream.write_all(&request_buf)?;
     Ok(())
 }
--- src/resp.rs
+++ src/resp.rs
@@ -1,30 +1,76 @@
-use std::fmt;
+use std::{fmt, io::{Bytes, Read}};
 
 #[derive(Debug, PartialEq)]
 pub enum Value {
-  SimpleString(String),
+  SimpleString(Vec<u8>),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum DecodeError {
+  IOError,
+  UnexpectedEOF,
+  UnexpectedType,
 }
 
 impl fmt::Display for DecodeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+          DecodeError::IOError => write!(f, "io error"),
+          DecodeError::UnexpectedEOF => write!(f, "unexpected eof"),
+          DecodeError::UnexpectedType => write!(f, "unexpected type"),
         }
     }
 }
 
 impl std::error::Error for DecodeError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-        }
+        None
     }
 }
 
-pub fn decode(_bytes: &[u8]) -> Result<Value, DecodeError> {
-  Ok(Value::SimpleString("OK".to_string()))
+pub fn decode(bytes: &[u8]) -> Result<Value, DecodeError> {
+  decode_value(&mut bytes.bytes())
+}
+
+fn decode_value(bytes: &mut Bytes<&[u8]>) -> Result<Value, DecodeError> {
+  match bytes.next() {
+    Some(Ok(b)) => {
+      match b {
+        b'+' => decode_simple_string(bytes),
+        _ => Err(DecodeError::UnexpectedType),
+      }
+    },
+    Some(Err(e)) => {
+      println!("decode_value error: {:?}", e);
+      Err(DecodeError::IOError)
+    },
+    None => {
+      Err(DecodeError::UnexpectedEOF)
+    }
+  }
+}
+
+fn decode_simple_string(bytes: &mut Bytes<&[u8]>) -> Result<Value, DecodeError> {
+  let mut v: Vec<u8> = Vec::new();
+  loop {
+    match bytes.next() {
+      Some(Ok(b'\r')) => {
+        bytes.next(); // drop LF(\n)
+        break;
+      },
+      Some(Ok(b)) => {
+        v.push(b);
+      },
+      Some(Err(e)) => {
+        println!("decode_simple_string error: {:?}", e);
+        return Err(DecodeError::IOError)
+      },
+      None => {
+        return Err(DecodeError::UnexpectedEOF)
+      }
+    }
+  }
+  Ok(Value::SimpleString(v))
 }
 
 #[cfg(test)]
@@ -35,6 +81,6 @@ mod tests {
     fn test_decode_simple_string() {
         let bytes = b"+OK\r\n";
         let result = decode(bytes);
-        assert_eq!(result, Ok(Value::SimpleString("OK".to_string())))
+        assert_eq!(result, Ok(Value::SimpleString(vec![b'O', b'K'])))
     }
 }
